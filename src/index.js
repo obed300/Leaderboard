c1a4f3a4b41e472f6deae3494cc560e2ddfb8ce6
obed300 <obamfo@st.ug.edu.gh>
2023-02-03 16:28:39 +0000

    Make changes

--- src/index.js
+++ src/index.js
@@ -1,27 +1,5 @@
 import './style.css';
-import Score from "./modules/class.js";
-import UI from "./modules/func.js";
+import Addnew from './modules/add.js';
 
-const form = document.querySelector('.form');
-const nameinput = document.querySelector('.name-input');
-const scoreinput = document.querySelector('.score-input');
-const refresh = document.querySelector('.refresh');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let name = nameinput.value;
-    let score = scoreinput.value;
-    const scores = new Score(name,score);
-    UI.libraryScores(scores);
-    UI.addScore(scores);
-});
-
-refresh.addEventListener('click', (e) =>{
-    e.preventDefault();
-    UI.displayScores();
-})
-
-document.addEventListener('DOMContentLoaded',(e)=>{
-    e.preventDefault();
-    UI.displayScores();
-});
+Addnew();
---
+++ src/modules/display.js
@@ -0,0 +1,34 @@
+import ScoresList from './classlist.js';
+
+export const score = new ScoresList();
+
+const createScore = async () => {
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/U048O1pn604XSuQgI6WO/scores');
+  const data = await response.json();
+  const { length } = data.result;
+  const listSection = document.querySelector('.results');
+  listSection.replaceChildren();
+  if (length > 0) {
+    listSection.style.display = 'block';
+    const listContainer = document.createElement('ul');
+    listContainer.className = 'allscores';
+    listSection.appendChild(listContainer);
+    for (let i = 0; i < length; i += 1) {
+      const list = document.createElement('li');
+      list.className = 'scoreItem';
+
+      const descrpt = document.createElement('p');
+      descrpt.id = i;
+      descrpt.textContent = `${data.result[i].user}: ${data.result[i].score}`;
+      list.appendChild(descrpt);
+
+      listContainer.append(list);
+    }
+    listSection.appendChild(listContainer);
+  }
+};
+
+const refresh = document.getElementById('refresh');
+refresh.addEventListener('click', createScore);
+
+export default createScore;
